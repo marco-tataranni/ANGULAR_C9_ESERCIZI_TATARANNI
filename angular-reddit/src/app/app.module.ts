7204ce126b8fe2db8a654b36b036e5fe6f154fbe
marco-tataranni <45785062+marco-tataranni@users.noreply.github.com>
2019-03-03 23:10:12 +0100

    angular-reddit

--- angular-reddit/src/app/app.module.ts
+++ angular-reddit/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
-
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; //Aggiungiamo qui 
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule, NgbModule //Aggiungiamolo qui
+    BrowserModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-reddit/src/app/article/article.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'card mt-4';
+  @Input() article:Article
+
+  constructor() {
+  }
+
+  voteUp(): Boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown():Boolean {
+    this.article.voteDown();
+    return false; 
+  }
+
+  ngOnInit() {}
+}
